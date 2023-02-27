3c9efed2579880cf36546cdc18fdcb222859289b
Hiba Trifi <trifi.hiba.solicode@gmail.com>
2023-02-27 12:05:39 +0000

    l

--- script.sql
+++ script.sql
@@ -45,3 +45,77 @@ CREATE TABLE Image(
    FOREIGN KEY(Id_ac) REFERENCES Annonce(Id_ac)
 );
 )
+
+
+--  insert into client
+INSERT INTO Client (Id_cl, Nom_cl, Prenom_cl, Email_cl, Password, Num_Tel) VALUES (1, 'Doe', 'John', 'johndoe@example.com', 'password123', '555-555-1234');
+
+--  insert into categorie
+INSERT INTO `categorie` (`Id_cat`, `Nom_cat`) VALUES ('1', 'Vente'), ('2', 'Location')
+
+--  insert into type
+INSERT INTO `type` (`Id_tp`, `Titre_tp`) VALUES ('1', 'Appartement'), ('2', 'Maison'), ('3', 'Villa'), ('4', 'Bureau'), ('5', 'Terrain');
+
+
+-- insert into annonce
+INSERT INTO Annonce (Id_ac, Titre, Prix, Date_pub, Date_mod, Adresse, Id_tp, Id_cat, Id_cl)
+VALUES (1, 'Appartement brtma', 39.99, '2022-02-14', '2022-02-14', '123 Main St.', 1, 1, 1);
+
+INSERT INTO Annonce (Id_ac, Titre, Prix, Date_pub, Date_mod, Adresse, Id_tp, Id_cat, Id_cl)
+VALUES (2, 'dar', 59.99, '2022-02-13', '2022-02-13', '456 Elm St.', 2, 2, 2);
+
+INSERT INTO Annonce (Id_ac, Titre, Prix, Date_pub, Date_mod, Adresse, Id_tp, Id_cat, Id_cl)
+VALUES (3, 'villa zwina', 79.99, '2022-02-12', '2022-02-12', '789 Oak St.', 3,3, 3);
+
+INSERT INTO Annonce (Id_ac, Titre, Prix, Date_pub, Date_mod, Adresse, Id_tp, Id_cat, Id_cl)
+VALUES (4, 'birouuu ', 19.99, '2022-02-11', '2022-02-11', '321 Pine St.', 4, 4,4);
+
+INSERT INTO Annonce (Id_ac, Titre, Prix, Date_pub, Date_mod, Adresse, Id_tp, Id_cat, Id_cl)
+VALUES (5, 'tirraaann', 99.99, '2022-02-10', '2022-02-10', '654 Maple St.', 5, 5, 5);
+
+-- inset into image 
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (1, 'Red bicycle front view', true, 1);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (2, 'Blue bicycle side view', false, 1);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (3, 'Yellow bicycle back view', false, 1);
+
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (4, 'Green bicycle angled view', true, 2);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (5, 'Orange bicycle top view', false, 2);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (6, 'Orange bicycle top view', false, 2);
+
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (7, 'Green bicycle angled view', true, 3);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (8, 'Orange bicycle top view', false, 3);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (9, 'Orange bicycle top view', false, 3);
+
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (10, 'Green bicycle angled view', true, 4);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (11, 'Orange bicycle top view', false, 4);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (12, 'Orange bicycle top view', false, 4);
+
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (13, 'Green bicycle angled view', true, 5);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (14, 'Orange bicycle top view', false, 5);
+INSERT INTO Image (Id_img, Titre_img, Img_Principale, Id_ac)
+VALUES (15, 'Orange bicycle top view', false, 5);
+
+-- Inner join
+SELECT client.Id_cl, client.Nom_cl , annonce.Id_ac, annonce.Titre FROM client INNER JOIN annonce ON client.Id_cl=annonce.Id_cl
+
+-- left join 
+SELECT client.Id_cl, client.Nom_cl , annonce.Id_ac, annonce.Titre FROM client LEFT JOIN annonce ON client.Id_cl=annonce.Id_cl
+
+-- right join 
+SELECT client.Id_cl, client.Nom_cl , annonce.Id_ac, annonce.Titre FROM client RIGHT JOIN annonce ON client.Id_cl=annonce.Id_cl
+
+
